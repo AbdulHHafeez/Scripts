3ea9679acf7bd86844199fe6dbeae4307e8fb345
AbdulHHafeez <99711215+AbdulHHafeez@users.noreply.github.com>
2022-02-20 02:51:39 -0500

    Update AddHWIDHash.ps1

--- AddHWIDHash.ps1
+++ AddHWIDHash.ps1
@@ -1,9 +1,12 @@
 <#
+Info:
+  This script can be used at OOBE screen of Windows 10 to add it as a Windows Autopilot device.
 How to use this script:
   1. Open an Administrator PowerShell window
   2. Type the following comands:
      - Set-ExecutionPolicyBypass
      - Invoke-WebRequest -Uri bit.ly/addhwidhash -OutFile .\script.ps1; .\script.ps1
+  3. Enter credentials
 #>
 $env:Path = "C:\Program Files\WindowsPowerShell\Scripts"
 Install-PackageProvider -Name NuGet -Force
