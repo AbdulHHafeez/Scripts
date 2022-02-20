64c385f4c2354b12a3a543b435c0852a74210008
AbdulHHafeez <99711215+AbdulHHafeez@users.noreply.github.com>
2022-02-20 02:09:14 -0500

    Add files via upload

---
+++ AddHWIDHash.ps1
@@ -0,0 +1,6 @@
+Set-ExecutionPolicy Bypass
+$env:Path = "C:\Program Files\WindowsPowerShell\Scripts"
+Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
+Install-PackageProvider NuGet
+Install-Script Get-WindowsAutoPilotInfo
+Get-WindowsAutoPilotInfo.ps1 -Online
