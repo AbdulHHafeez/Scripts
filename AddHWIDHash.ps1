6457a204c9852c7ff3137857ba9df3b690830809
AbdulHHafeez <99711215+AbdulHHafeez@users.noreply.github.com>
2022-02-20 02:38:15 -0500

    Update AddHWIDHash.ps1

--- AddHWIDHash.ps1
+++ AddHWIDHash.ps1
@@ -1,5 +1,5 @@
 $env:Path = "C:\Program Files\WindowsPowerShell\Scripts"
-Install-PackageProvider NuGet
+Install-PackageProvider NuGet -force
 Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
-Install-Script Get-WindowsAutoPilotInfo
+Install-Script Get-WindowsAutoPilotInfo -force
 Get-WindowsAutoPilotInfo.ps1 -Online
