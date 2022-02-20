dd03938e303423f57986770b450731599bf62b5c
AbdulHHafeez <99711215+AbdulHHafeez@users.noreply.github.com>
2022-02-20 02:46:12 -0500

    Update AddHWIDHash.ps1

--- AddHWIDHash.ps1
+++ AddHWIDHash.ps1
@@ -1,3 +1,10 @@
+<#
+How to use this script:
+  1. Open an Administrator PowerShell window
+  2. Type the following comands:
+     - Set-ExecutionPolicyBypass
+     - Invoke-WebRequest -Uri bit.ly/addhwidhash -OutFile .\script.ps1; .\script.ps1
+#>
 $env:Path = "C:\Program Files\WindowsPowerShell\Scripts"
 Install-PackageProvider -Name NuGet -Force
 Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
